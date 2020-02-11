5afb5560fb6a1294a7dc749cea3b639549323eb8
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-11 13:03:46 +0300

    Update _form.php

--- views/default/_form.php
+++ views/default/_form.php
@@ -9,40 +9,26 @@ use artsoft\helpers\Html;
 /* @var $form artsoft\widgets\ActiveForm */
 ?>
 
-<div class="slides-form">
+    <div class="slides-form">
 
-    <?php
-    $form = ActiveForm::begin([
-                'id' => 'slides-form',
-                'validateOnBlur' => false,
-            ])
-    ?>
+        <?php
+        $form = ActiveForm::begin([
+            'id' => 'slides-form',
+            'validateOnBlur' => false,
+        ])
+        ?>
 
-    <div class="row">
-        <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>   
-                            
-                            <?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(Slides::getStatusList()) ?>                          
-                        </div>                        
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <?php if (!$model->isNewRecord) : ?>
-                        <?= artsoft\slides\widgets\SlidesLayersWidget::widget(['model' => $model]); ?>
-                    <?php endif; ?>
-                </div>
-
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">                   
+                                <?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(Slides::getStatusList()) ?>
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-md-4">
                                 <?= $form->field($model, 'data_transition')->textInput(['maxlength' => true]) ?>
@@ -50,11 +36,10 @@ use artsoft\helpers\Html;
                             <div class="col-md-4">
                                 <?= $form->field($model, 'data_slotamount')->textInput() ?>
                             </div>
-                             <div class="col-md-4">
+                            <div class="col-md-4">
                                 <?= $form->field($model, 'data_masterspeed')->textInput() ?>
                             </div>
-                        </div>  
-                       
+                        </div>
                         <div class="row">
                             <div class="col-md-4">
                                 <?= $form->field($model, 'data_fullwidthcentering')->textInput(['maxlength' => true]) ?>
@@ -62,10 +47,10 @@ use artsoft\helpers\Html;
                             <div class="col-md-4">
                                 <?= $form->field($model, 'data_bgfit')->textInput(['maxlength' => true]) ?>
                             </div>
-                             <div class="col-md-4">
-                                 <?= $form->field($model, 'data_delay')->textInput() ?> 
+                            <div class="col-md-4">
+                                <?= $form->field($model, 'data_delay')->textInput() ?>
                             </div>
-                        </div> 
+                        </div>
                         <div class="row">
                             <div class="col-md-6">
                                 <?= $form->field($model, 'data_bgposition')->textInput(['maxlength' => true]) ?>
@@ -73,117 +58,82 @@ use artsoft\helpers\Html;
                             <div class="col-md-6">
                                 <?= $form->field($model, 'data_bgrepeat')->textInput(['maxlength' => true]) ?>
                             </div>
-                        </div> 
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <?php if (!$model->isNewRecord) : ?>
+                                    <?= artsoft\slides\widgets\SlidesLayersWidget::widget(['model' => $model]); ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-body">    
-                    <div class="record-info">
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;"><?= $model->attributeLabels()['id'] ?>: </label>
-                                <span><?= $model->id ?></span>
-                            </div>
-                        <?php if (!$model->isNewRecord): ?>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['created_at'] ?> :
-                                </label>
-                                <span><?= $model->createdDatetime ?></span>                                
-                            </div>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['updated_at'] ?> :
-                                </label>
-                                <span><?= $model->updatedDatetime ?></span>
-                            </div>
-
-                            <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['updated_by'] ?> :
-                                </label>
-                                <span><?= $model->updatedBy->username ?></span>
-                            </div>
-                        <?php endif; ?>
-
-
-                        <div class="form-group">
-                            <?php if ($model->isNewRecord): ?>
-                                <?= Html::submitButton(Yii::t('art', 'Create'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Cancel'), ['/slides/default/index'], ['class' => 'btn btn-default']) ?>
-                            <?php else: ?>
-                                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-                                <?=
-                                Html::a(Yii::t('art', 'Delete'), ['/slides/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-danger',
-                                    'data' => [
-                                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'method' => 'post',
-                                    ],
-                                ])
-                                ?>
-                            <?php endif; ?>
+                    <div class="col-md-4">
+                        <div class="row">
+                            <?= $form->field($model, 'img_src')->widget(\artsoft\media\widgets\FileInput::className(), [
+                                'name' => 'image',
+                                'buttonTag' => 'button',
+                                'buttonName' => Yii::t('art', 'Browse'),
+                                'buttonOptions' => ['class' => 'btn btn-primary btn-file-input'],
+                                'options' => ['class' => 'form-control'],
+                                'template' => '<div class="slides-img_src thumbnail"></div><div class="input-group"><span class="input-group-btn">{button}</span>{input}</div>',
+                                'thumb' => 'original',
+                                'imageContainer' => '.slides-img_src',
+                                'pasteData' => \artsoft\media\widgets\FileInput::DATA_URL,
+                                'callbackBeforeInsert' => 'function(e, data) {
+                                $(".img_src-thumbnail").show();
+                            }',
+                            ])
+                            ?>
+                            <?= $form->field($model, 'data_lazyload')->widget(\artsoft\media\widgets\FileInput::className(), [
+                                'name' => 'image',
+                                'buttonTag' => 'button',
+                                'buttonName' => Yii::t('art', 'Browse'),
+                                'buttonOptions' => ['class' => 'btn btn-primary btn-file-input'],
+                                'options' => ['class' => 'form-control'],
+                                'template' => '<div class="slides-data_lazyload thumbnail"></div><div class="input-group"><span class="input-group-btn">{button}</span>{input}</div>',
+                                'thumb' => 'original',
+                                'imageContainer' => '.slides-data_lazyload',
+                                'pasteData' => \artsoft\media\widgets\FileInput::DATA_URL,
+                                'callbackBeforeInsert' => 'function(e, data) {
+                                $(".data_lazyload-thumbnail").show();
+                            }',
+                            ])
+                            ?>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-                        
-                        <?= $form->field($model, 'img_src')->widget(\artsoft\media\widgets\FileInput::className(), [
-                            'name' => 'image',
-                            'buttonTag' => 'button',
-                            'buttonName' => Yii::t('art', 'Browse'),
-                            'buttonOptions' => ['class' => 'btn btn-primary btn-file-input'],
-                            'options' => ['class' => 'form-control'],
-                            'template' => '<div class="slides-img_src thumbnail"></div><div class="input-group"><span class="input-group-btn">{button}</span>{input}</div>',
-                            'thumb' => 'original',
-                            'imageContainer' => '.slides-img_src',
-                            'pasteData' => \artsoft\media\widgets\FileInput::DATA_URL,
-                            'callbackBeforeInsert' => 'function(e, data) {
-                                $(".img_src-thumbnail").show();
-                            }',
+            <div class="panel-footer">
+                <div class="form-group">
+                    <?= Html::a(Yii::t('art', 'Go to list'), ['/slides/default/index'], ['class' => 'btn btn-default']) ?>
+                    <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
+                    <?php if (!$model->isNewRecord): ?>
+                        <?=
+                        Html::a(Yii::t('art', 'Delete'), ['/slides/default/delete', 'id' => $model->id], [
+                            'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                'method' => 'post',
+                            ],
                         ])
                         ?>
-                        <?= $form->field($model, 'data_lazyload')->widget(\artsoft\media\widgets\FileInput::className(), [
-                            'name' => 'image',
-                            'buttonTag' => 'button',
-                            'buttonName' => Yii::t('art', 'Browse'),
-                            'buttonOptions' => ['class' => 'btn btn-primary btn-file-input'],
-                            'options' => ['class' => 'form-control'],
-                            'template' => '<div class="slides-data_lazyload thumbnail"></div><div class="input-group"><span class="input-group-btn">{button}</span>{input}</div>',
-                            'thumb' => 'original',
-                            'imageContainer' => '.slides-data_lazyload',
-                            'pasteData' => \artsoft\media\widgets\FileInput::DATA_URL,
-                            'callbackBeforeInsert' => 'function(e, data) {
-                                $(".data_lazyload-thumbnail").show();
-                            }',
-                        ])
-                        ?>
-
-                    </div>
+                    <?php endif; ?>
                 </div>
+                <?= \artsoft\widgets\InfoModel::widget(['model' => $model]); ?>
             </div>
         </div>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
 
-    <?php ActiveForm::end(); ?>
-
-</div>
-
-<!--        модал добавления слоя в слайд-->
+    <!--        модал добавления слоя в слайд-->
 <?php
 \yii\bootstrap\Modal::begin([
     'header' => '<h3 class="page-title">' . Yii::t('art/slides', 'Add Layers') . '</h3>',
     'size' => 'modal-lg',
     'id' => 'slides-layers-modal',
-        //'footer' => 'footer',
+    //'footer' => 'footer',
 ]);
 
 \yii\bootstrap\Modal::end();
